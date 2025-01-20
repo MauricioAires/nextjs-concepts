450c5d5e11379d6dd52a34ca3830ad7a97a169a3
MauricioAires <mauricioaires015@gmail.com>
2025-01-19 22:40:04 -0300

    :sparkles: config prisma with next-auth

--- examples/auth/auth.ts
+++ examples/auth/auth.ts
@@ -3,7 +3,18 @@ import NextAuth from "next-auth";
 
 import google from "next-auth/providers/google";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 const config: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+    // O padrão do next-auth é o session.
+    // Session é mais seguro mas o Prisma ainda não está atualizado para utilizar o session.
+  },
   providers: [google],
   callbacks: {
     authorized({ request, auth }) {
